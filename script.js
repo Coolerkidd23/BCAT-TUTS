b07eba77b043f74f8d53d09a70e3a217692668d4
Coolerkidd23 <masebole945@gmail.com>
2025-07-23 06:17:35 +0200

    final edits

---
+++ script.js
@@ -0,0 +1,10 @@
+
+
+document.addEventListener('DOMContentLoaded', () => {
+  const hamburger = document.getElementById('hamburger');
+  const navLinks = document.getElementById('nav-links');
+
+  hamburger.addEventListener('click', () => {
+    navLinks.style.display = navLinks.style.display === 'flex' ? 'none' : 'flex';
+  });
+});
